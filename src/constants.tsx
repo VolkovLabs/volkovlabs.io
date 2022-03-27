7adb7eb20f29a4e997ca5ce11a6f4cae1fd01370
Mikhail <47795110+mikhailredis@users.noreply.github.com>
2022-03-26 21:04:36 -0400

    Update Plugins and Colors

--- src/constants.tsx
+++ src/constants.tsx
@@ -15,8 +15,8 @@ export const PluginList: FeatureItem[] = [
       </>
     ),
     href: "/plugins/volkovlabs-image-panel",
-    badge: "https://img.shields.io/badge/open-source-blue?style=for-the-badge",
-    badge2: "https://img.shields.io/badge/community-green?style=for-the-badge",
+    badge: "https://img.shields.io/badge/open-source-black?style=for-the-badge",
+    badge2: "https://img.shields.io/badge/community-9d70f9?style=for-the-badge",
   },
   {
     title: "RSS/Atom Data Source",
@@ -29,8 +29,8 @@ export const PluginList: FeatureItem[] = [
       </>
     ),
     href: "/plugins/volkovlabs-rss-datasource",
-    badge: "https://img.shields.io/badge/open-source-blue?style=for-the-badge",
-    badge2: "https://img.shields.io/badge/community-green?style=for-the-badge",
+    badge: "https://img.shields.io/badge/open-source-black?style=for-the-badge",
+    badge2: "https://img.shields.io/badge/community-9d70f9?style=for-the-badge",
   },
   {
     title: "Environment Data Source",
@@ -43,12 +43,12 @@ export const PluginList: FeatureItem[] = [
       </>
     ),
     href: "/plugins/volkovlabs-env-datasource",
-    badge: "https://img.shields.io/badge/open-source-blue?style=for-the-badge",
-    badge2: "https://img.shields.io/badge/private-red?style=for-the-badge",
+    badge: "https://img.shields.io/badge/open-source-black?style=for-the-badge",
+    badge2: "https://img.shields.io/badge/private-ff5656?style=for-the-badge",
   },
   {
     title: "Mesmerizing Flows NFT",
-    png: "/img/plugins/mesmerizing-flows-nft.png",
+    Svg: require("@site/static/img/plugins/mesmerizing-flows.svg").default,
     description: (
       <>
         Every flow is unique and created using Volkov Labs plugins for Grafana.
@@ -56,11 +56,11 @@ export const PluginList: FeatureItem[] = [
     ),
     target: "_blank",
     href: "https://solsea.io/collection/61ce604b558d7a2f0b8ecacc",
-    badge2: "https://img.shields.io/badge/private-red?style=for-the-badge",
+    badge2: "https://img.shields.io/badge/private-ff5656?style=for-the-badge",
   },
   {
     title: "Redis Data Source and Application",
-    png: "/img/plugins/redisgrafana.png",
+    Svg: require("@site/static/img/plugins/redisgrafana.svg").default,
     description: (
       <>
         The Redis plugins for Grafana let you connect Grafana to Redis to
@@ -70,18 +70,18 @@ export const PluginList: FeatureItem[] = [
     ),
     target: "_blank",
     href: "https://redisgrafana.github.io",
-    badge: "https://img.shields.io/badge/open-source-blue?style=for-the-badge",
-    badge2: "https://img.shields.io/badge/community-green?style=for-the-badge",
+    badge: "https://img.shields.io/badge/open-source-black?style=for-the-badge",
+    badge2: "https://img.shields.io/badge/community-9d70f9?style=for-the-badge",
   },
   {
     title: "Molycop",
-    png: "/img/plugins/molycop.png",
+    Svg: require("@site/static/img/plugins/molycop.svg").default,
     description: (
       <>The Mill Charge Motion Widget to display sensor information.</>
     ),
     target: "_blank",
     href: "https://molycop.com/",
-    badge2: "https://img.shields.io/badge/private-red?style=for-the-badge",
+    badge2: "https://img.shields.io/badge/private-ff5656?style=for-the-badge",
   },
   {
     title: "Theiascope",
@@ -94,6 +94,6 @@ export const PluginList: FeatureItem[] = [
     ),
     target: "_blank",
     href: "https://www.theiascope.io/",
-    badge2: "https://img.shields.io/badge/private-red?style=for-the-badge",
+    badge2: "https://img.shields.io/badge/private-ff5656?style=for-the-badge",
   },
 ];
