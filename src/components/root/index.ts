e45e70686cfc6c181fe269edcae5e16176752d7a
rtemision <3108511+rtemision@users.noreply.github.com>
2022-04-29 18:15:03 +0700

    typography variables

---
+++ src/components/root/index.ts
@@ -0,0 +1,5 @@
+import './_theme/root_theme_default.post.css';
+import './_typography/root_typography_xs-default.post.css';
+import './_typography/root_typography_sm-default.post.css';
+import './_typography/root_typography_md-default.post.css';
+import './_typography/root_typography_lg-default.post.css';
