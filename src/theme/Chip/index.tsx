08a1527364d217ba9001e1109713589c13132c4c
Mikhail <mikhail@volkovlabs.io>
2023-01-22 08:50:53 -0500

    Add Chip and update Screenshot

---
+++ src/theme/Chip/index.tsx
@@ -0,0 +1,57 @@
+import clsx from 'clsx';
+import React from 'react';
+import Link from '@docusaurus/Link';
+import styles from './styles.module.css';
+
+type Skin = "primary" | "secondary";
+type Size = "small" | "medium" | "large";
+
+/**
+ * Properties
+ */
+type Props = {
+  label: string;
+  permalink: string;
+  className?: string;
+  skin?: Skin;
+  size?: Size;
+  active?: boolean;
+};
+
+/**
+ * Skins
+ */
+const skins: { [key in Skin]: string } = {
+  primary: styles.skinPrimary,
+  secondary: styles.skinSecondary,
+};
+
+/**
+ * Sizes
+ */
+const sizes: { [key in Size]: string } = {
+  small: styles.sizeSmall,
+  medium: styles.sizeMedium,
+  large: styles.sizeLarge,
+};
+
+/**
+ * Chip
+ */
+export const Chip = ({
+  label,
+  permalink,
+  className,
+  skin = "primary",
+  size = "small",
+  active,
+}: Props) => (
+  <Link
+    className={clsx(styles.root, className, skins[skin], sizes[size], {
+      [styles.active]: active,
+    })}
+    to={permalink}
+  >
+    {label}
+  </Link>
+);
