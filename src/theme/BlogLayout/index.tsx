71b7fe8a40007fa565e203469e6a5216a0837094
Mikhail <mikhail@volkovlabs.io>
2023-01-23 06:13:44 -0500

    Update blogs and fix style

--- src/theme/BlogLayout/index.tsx
+++ src/theme/BlogLayout/index.tsx
@@ -11,10 +11,27 @@ const BlogLayout = (props) => {
     <Layout {...layoutProps}>
       <div className="container margin-vert--lg">
         <div className="row">
-          <main className="col" itemScope itemType="http://schema.org/Blog">
-            {children}
-          </main>
-          {toc && <div className="col col--2">{toc}</div>}
+          {!toc && (
+            <main
+              className="col col--12"
+              itemScope
+              itemType="http://schema.org/Blog"
+            >
+              {children}
+            </main>
+          )}
+          {toc && (
+            <>
+              <main
+                className="col col--10"
+                itemScope
+                itemType="http://schema.org/Blog"
+              >
+                {children}
+              </main>
+              <div className="col col--2">{toc}</div>
+            </>
+          )}
         </div>
       </div>
     </Layout>
