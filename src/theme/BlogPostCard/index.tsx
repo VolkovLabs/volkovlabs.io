bb476e92fc7bd7090d2b17f994d3ea307c97383a
Daria Volkova <94922548+daria-vl@users.noreply.github.com>
2023-01-23 14:29:15 -0500

    PostgreSQL in Grafana blog post (#187)
    * PostgreSQL in Grafana blog post
    
    * Updates
    
    * added introduction to the first article
    
    * Why PostgreSQL blog post, draft 1
    
    * Update blogs
    
    * Updates
    
    * Update configuration to setup
    
    * Update blog
    
    * Ultimate storage partner blog post update
    
    * PostgreSQL is ultimate partner blog post update
    
    * Separe Blog Posts from Card
    
    * Update banners
    
    * Update blogs
    
    * Add banners
    
    * Add banner
    
    * Update blogs
    
    * Update blog
    
    * Update blogs
    
    * Update banners
    
    * Update blog
    
    Co-authored-by: Mikhail <mikhail@volkovlabs.io>

---
+++ src/theme/BlogPostCard/index.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import Link from '@docusaurus/Link';
+import { Chip } from '../Chip';
+import styles from './styles.module.css';
+
+/**
+ * Blog Card
+ */
+const BlogPostCard = ({ post }) => {
+  const { permalink, title, formattedDate, frontMatter, tags, authors } =
+    post.metadata;
+  const author = authors[0];
+  const tag = tags[0];
+
+  return (
+    <div className={styles.post}>
+      <div className={styles.imageBox}>
+        <Link to={permalink} className={styles.image}>
+          <img loading="lazy" src={frontMatter.image} alt={title} />
+        </Link>
+      </div>
+
+      <div className={styles.content}>
+        <div className={styles.tags}>
+          <Chip label={tag.label} permalink={tag.permalink} skin="secondary" />
+        </div>
+
+        <h3 className={styles.title}>
+          <Link to={permalink}>{title}</Link>
+        </h3>
+
+        <div className={styles.author}>
+          by <b>{author.name}</b> on {formattedDate}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default BlogPostCard;
