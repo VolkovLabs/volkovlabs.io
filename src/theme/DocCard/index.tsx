3adc51e7de5edbae24c9dae42dd2f3efb6021564
Mikhail <mikhail@volkovlabs.io>
2023-02-07 14:35:16 -0500

    Add Doc Card and List

---
+++ src/theme/DocCard/index.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import Link from '@docusaurus/Link';
+import { Chip } from '../Chip';
+import styles from './styles.module.css';
+
+/**
+ * Properties
+ */
+type Props = {
+  doc: any;
+};
+
+/**
+ * Doc Card
+ */
+const DocCard = ({ doc }: Props) => {
+  const { permalink, title, description } = doc;
+
+  return (
+    <div className={styles.post}>
+      <div className={styles.content}>
+        <h3 className={styles.title}>
+          <Link to={permalink}>{title}</Link>
+        </h3>
+        {description && <div className={styles.description}>{description}</div>}
+      </div>
+    </div>
+  );
+};
+
+export default DocCard;
