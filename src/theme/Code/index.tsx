b1b670e737506b60b4519f4c293fc1fc97a4991e
Mikhail <mikhail@volkovlabs.io>
2023-02-12 10:20:31 -0500

    Add Them Code Component

---
+++ src/theme/Code/index.tsx
@@ -0,0 +1,54 @@
+import React, { useReducer } from 'react';
+import CodeBlock from '@theme-init/CodeBlock';
+import { codeReducer, fetchCode, initialFetchResultState } from './utils';
+
+/**
+ * Properties
+ */
+type Props = {
+  url?: string;
+  language: string;
+  title: string;
+};
+
+/**
+ * Code
+ */
+const Code = ({ url, language, title }: Props) => {
+  const [fetchResultState, fetchResultStateDispatcher] = useReducer(
+    codeReducer,
+    initialFetchResultState
+  );
+
+  /**
+   * Parse URL
+   */
+  const [org, repo, blob, branch, ...pathSeg] = new global.URL(url).pathname
+    .split("/")
+    .slice(1);
+
+  /**
+   * Content
+   */
+  const content = {
+    url: `https://raw.githubusercontent.com/${org}/${repo}/${branch}/${pathSeg.join(
+      "/"
+    )}`,
+    title: pathSeg.join("/"),
+  };
+
+  /**
+   * Loading
+   */
+  if (fetchResultState.loading !== false) {
+    fetchCode(content, fetchResultStateDispatcher);
+  }
+
+  return (
+    <CodeBlock language={language} title={title ? title : content.title}>
+      {fetchResultState.code}
+    </CodeBlock>
+  );
+};
+
+export default Code;
---
+++ src/theme/Code/utils.ts
@@ -0,0 +1,93 @@
+/**
+ * GitHub Reference
+ */
+interface GitHubReference {
+  url: string;
+  title: string;
+}
+
+/**
+ * Dispatch Types
+ */
+type DispatchTypes = "reset" | "loading" | "loaded" | "error";
+
+/**
+ * Dispatch Message
+ */
+export interface DispatchMessage {
+  type: DispatchTypes;
+  value: string | Error;
+}
+
+/**
+ * Initial State
+ */
+export const initialFetchResultState = {
+  code: "loading...",
+  loading: null,
+};
+
+/**
+ * Fetch Code
+ */
+export const fetchCode = async (
+  { url }: GitHubReference,
+  fetchResultStateDispatcher: React.Dispatch<DispatchMessage>
+) => {
+  let res: Response;
+
+  try {
+    res = await fetch(url);
+  } catch (err) {
+    return fetchResultStateDispatcher({ type: "error", value: err });
+  }
+
+  if (res.status !== 200) {
+    const error = await res.text();
+    return fetchResultStateDispatcher({ type: "error", value: error });
+  }
+
+  const body = (await res.text()).split("\n");
+  const preceedingSpace = body.reduce((prev: number, line: string) => {
+    if (line.length === 0) {
+      return prev;
+    }
+
+    const spaces = line.match(/^\s+/);
+    if (spaces) {
+      return Math.min(prev, spaces[0].length);
+    }
+
+    return 0;
+  }, Infinity);
+
+  return fetchResultStateDispatcher({
+    type: "loaded",
+    value: body.map((line) => line.slice(preceedingSpace)).join("\n"),
+  });
+};
+
+/**
+ * Code Reducer
+ */
+export const codeReducer = (
+  prevState: any,
+  { type, value }: DispatchMessage
+) => {
+  switch (type) {
+    case "reset": {
+      return initialFetchResultState;
+    }
+    case "loading": {
+      return { ...prevState, loading: true };
+    }
+    case "loaded": {
+      return { ...prevState, code: value, loading: false };
+    }
+    case "error": {
+      return { ...prevState, code: value, loading: false };
+    }
+    default:
+      return prevState;
+  }
+};
