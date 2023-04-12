ccc40d514c7d658ff826367346bd04f1956e0fc3
Daria Volkova <94922548+daria-vl@users.noreply.github.com>
2023-04-11 21:50:31 -0400

    General docs edits (#334)
    * General docs edits
    
    * Update customize.mdx
    
    * Update annotations.mdx
    
    * Update customize.mdx
    
    * Update customize.mdx
    
    * Update json.mdx
    
    * Updates
    
    * Update index.mdx
    
    * Remove Link
    
    ---------
    
    Co-authored-by: Mikhail <mikhail@volkovlabs.io>

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,14 @@
-import clsx from "clsx";
-import React from "react";
-import Head from "@docusaurus/Head";
-import Link from "@docusaurus/Link";
-import useBaseUrl from "@docusaurus/useBaseUrl";
-import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
-import Layout from "@theme/Layout";
-import ThemedImage from "@theme/ThemedImage";
-import { Plugins, Projects } from "../components";
-import Blogs from "../components/Blogs";
-import styles from "../css/index.module.css";
-import Image from "../theme/Image";
+import clsx from 'clsx';
+import React from 'react';
+import Head from '@docusaurus/Head';
+import useBaseUrl from '@docusaurus/useBaseUrl';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import Layout from '@theme/Layout';
+import ThemedImage from '@theme/ThemedImage';
+import { Plugins, Projects } from '../components';
+import Blogs from '../components/Blogs';
+import styles from '../css/index.module.css';
+import Image from '../theme/Image';
 
 /**
  * Header
