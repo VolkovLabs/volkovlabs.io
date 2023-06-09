ec2e4ab0deec954a7a923813ef0249dab58197f0
Daria Volkova <94922548+daria-vl@users.noreply.github.com>
2023-06-08 20:48:20 -0400

    Course Edits - new structure (#407)
    * Course Edits - new structure
    
    * Updates
    
    ---------
    
    Co-authored-by: Mikhail <mikhail@volkovlabs.io>

--- config/sidebar/grafana.js
+++ config/sidebar/grafana.js
@@ -17,18 +17,30 @@ const mySidebar = [
     label: "Getting Started",
     type: "category",
     collapsible: false,
+    link: {
+      type: "doc",
+      id: "started",
+    },
   },
   {
     items: ["datasources/json", "storage", "iot"],
     label: "Advanced",
     type: "category",
     collapsible: false,
+    link: {
+      type: "doc",
+      id: "advanced/index",
+    },
   },
   {
     items: ["developer/panel"],
     label: "Developer",
     type: "category",
     collapsible: false,
+    link: {
+      type: "doc",
+      id: "developer/index",
+    },
   },
 ];
 
