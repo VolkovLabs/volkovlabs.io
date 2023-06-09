4a5ac8f5da58c5fb6f51956de45e8d95559e9a7b
daria-vl <daria@volkovlabs.io>
2023-06-08 20:36:24 -0400

    Course Edits - new structure

--- config/sidebar/grafana.js
+++ config/sidebar/grafana.js
@@ -17,19 +17,33 @@ const mySidebar = [
     label: "Getting Started",
     type: "category",
     collapsible: false,
+    link: {
+      type: "doc",
+      id: "started",
+    },
   },
   {
     items: ["datasources/json", "storage", "iot"],
     label: "Advanced",
     type: "category",
     collapsible: false,
+    link: {
+      type: "doc",
+      id: "advanced/index",
+    },
   },
   {
-    items: ["developer/panel"],
+    items: [
+      "developer/index",
+      "developer/panel"],
     label: "Developer",
     type: "category",
     collapsible: false,
-  },
+    link: {
+      type: "doc",
+      id: "developer/index",
+    },
+},
 ];
 
 /**
