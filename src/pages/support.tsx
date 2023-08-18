45b73b8a1400893398260888780793b08f09a40f
Mikhail Volkov <47795110+mikhail-vl@users.noreply.github.com>
2023-08-17 21:10:02 -0400

    Update Calendar 2.1.0 release (#490)

--- src/pages/support.tsx
+++ src/pages/support.tsx
@@ -72,13 +72,13 @@ const Main = (siteConfig) => {
           <div className="col col--6">
             <Image src="/img/team/daria.png" zoom={false} lazy={true} />
             <h3>Daria</h3>
-            <p>Creative Director / COO</p>
+            <p>Creative Director, COO</p>
           </div>
 
           <div className="col col--6">
             <Image src="/img/team/mikhail.png" zoom={false} lazy={true} />
             <h3>Mikhail</h3>
-            <p>Founder / CEO</p>
+            <p>Founder, CEO and Grafana Champion</p>
           </div>
         </div>
       </div>
