99c11be24efc8376609b319141227514e9427c98
Mikhail <mikhail@volkovlabs.io>
2023-09-17 20:29:26 -0400

    Update files for DM, Grapi and RSS

---
+++ docs/volkovlabs-form-panel/js/initial-request.js
@@ -0,0 +1,29 @@
+const bucketsSelect = elements.find((element) => element.id === "buckets");
+
+/**
+ * Set URL
+ */
+const url = `http://localhost:3001/test`;
+
+/**
+ * Fetch
+ */
+const resp = fetch(url, {
+  method: "GET",
+  headers: {
+    "Content-Type": "application/json",
+    "PRIVATE-TOKEN": "$token",
+  },
+})
+  .catch((error) => {
+    console.error(error);
+  })
+  .then(async (resp) => {
+    const body = await resp.json();
+
+    bucketsSelect.options = body.buckets.map((value) => {
+      return { label: value, value };
+    });
+
+    onOptionsChange(options);
+  });
