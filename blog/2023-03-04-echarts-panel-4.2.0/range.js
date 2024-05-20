329252303c2fa4be8000cc90b6e07c7863e25307
Mikhail Volkov <mikhail@volkovlabs.io>
2024-05-19 22:13:34 -0400

    Updates

--- blog/2023-03-04-echarts-panel-4.2.0/range.js
+++
@@ -1,9 +0,0 @@
-echartsInstance.off("datarangeselected");
-echartsInstance.on("datarangeselected", (params) => {
-  const startValue = params.selected[0];
-  const endValue = params.selected[1];
-  locationService.partial(
-    { "var-datarangeStart": startValue, "var-datarangeEnd": endValue },
-    true
-  );
-});
