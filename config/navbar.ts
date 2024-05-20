3febf58c5ec04105b50e223b03188c5a8b4fcd9a
Mikhail Volkov <mikhail@volkovlabs.io>
2024-05-19 22:43:52 -0400

    Updates

--- config/navbar.ts
+++ config/navbar.ts
@@ -134,6 +134,10 @@ export const NAVBAR = {
           to: "/blog/tags/business-intelligence",
         },
         {
+          label: "Community",
+          to: "/blog/tags/community",
+        },
+        {
           label: "Grafana Crash Course",
           to: "/blog/tags/grafana-crash-course",
         },
