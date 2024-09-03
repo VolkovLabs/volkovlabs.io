4e06e71445213f8ed1cf9958702110e230ed6037
Mikhail Volkov <mikhail@volkovlabs.io>
2024-09-03 19:28:54 -0400

    Remove java that cause build errors

--- config/theme.config.ts
+++ config/theme.config.ts
@@ -30,6 +30,6 @@ export const THEME_CONFIG: ThemeConfig = {
   prism: {
     theme: themes.github,
     darkTheme: themes.dracula,
-    additionalLanguages: ["docker", "nginx", "bash", "json", "yaml", "java"],
+    additionalLanguages: ["docker", "nginx", "bash", "json", "yaml"],
   },
 };
