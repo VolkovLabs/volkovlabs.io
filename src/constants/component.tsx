74c4c81b8b0c07de340da7bca1b80d763e0ef4db
Mikhail Volkov <47795110+mikhail-vl@users.noreply.github.com>
2024-10-09 19:40:53 -0400

    Update Business Intelligence 2.0.0 (#829)
    * Update Business Intelligence 2.0.0
    
    * Updates

--- src/constants/component.tsx
+++ src/constants/component.tsx
@@ -14,7 +14,7 @@ export const ComponentList: PluginItem[] = [
   {
     title: "Business Engine",
     name: "business-engine",
-    description: <>High performance backend</>,
+    description: <>High performance backend with HA</>,
     href: "/big/engine/",
   },
   {
