dd23bfbf83625416649b4326a84a2b8cbfe3c1e1
daria-vl <daria@volkovlabs.io>
2024-12-14 15:17:45 -0500

    Table 1.9.0 docs updates

--- config/plugins_sidebar.ts
+++ config/plugins_sidebar.ts
@@ -342,16 +342,23 @@ const sidebars: SidebarsConfig = {
           label: "Introduction",
         },
         {
-          items: ["business-table/add-delete", "business-table/permission"],
+          items: ["business-table/layout",
+                  "business-table/add",
+                  "business-table/edit",
+                  "business-table/editor-types",
+                  "business-table/delete",
+                  "business-table/nested",
+                  "business-table/permission"
+                ],
           label: "Editable data",
           type: "category",
           link: {
             type: "doc",
-            id: "business-table/edit",
+            id: "business-table/editable",
           },
         },
         {
-          items: ["business-table/nested"],
+          items: ["business-table/ds-as-var"],
           label: "Features",
           type: "category",
           link: {
