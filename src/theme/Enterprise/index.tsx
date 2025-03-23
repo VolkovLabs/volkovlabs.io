1c7614445d3850378213c465425c0da271aca1e9
Mikhail Volkov <mikhail@volkovlabs.io>
2025-03-23 19:06:07 -0400

    Update Grafana Crash Course

--- src/theme/Enterprise/index.tsx
+++ src/theme/Enterprise/index.tsx
@@ -2,9 +2,11 @@ import React from "react";
 import styles from "./styles.module.css";
 
 /**
- * Enterprise
+ * Enterprise Component
+ * Renders a styled button linking to the pricing page for Business Suite Enterprise
+ * Includes an SVG checkmark icon and text for enrollment
  */
-const Enterprise = () => (
+const Enterprise: React.FC = () => (
   <a className={styles.button} href="/pricing">
     <svg
       aria-hidden="true"
@@ -13,15 +15,15 @@ const Enterprise = () => (
       height="16"
       width="16"
       data-view-component="true"
-      class={styles.buttonSvg2}
+      className={styles.buttonSvg2}
     >
       <path
         d="M78.049,19.015L29.458,67.606c-0.428,0.428-1.121,0.428-1.548,0L0.32,40.015c-0.427-0.426-0.427-1.119,0-1.547l6.704-6.704
-		c0.428-0.427,1.121-0.427,1.548,0l20.113,20.112l41.113-41.113c0.429-0.427,1.12-0.427,1.548,0l6.703,6.704
-		C78.477,17.894,78.477,18.586,78.049,19.015z"
+        c0.428-0.427,1.121-0.427,1.548,0l20.113,20.112l41.113-41.113c0.429-0.427,1.12-0.427,1.548,0l6.703,6.704
+        C78.477,17.894,78.477,18.586,78.049,19.015z"
       />
     </svg>
-    &nbsp;&nbsp;Enroll in Business Suite Enterprise
+    &nbsp; Enroll in Business Suite Enterprise
   </a>
 );
 
