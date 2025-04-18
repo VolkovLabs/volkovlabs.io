36054829fdf6dc618503cb1307c77ee9b1e54a09
Daria Volkova <94922548+daria-vl@users.noreply.github.com>
2025-04-17 21:51:37 -0400

    Blog post BIG 300 (#956)
    * Blog post BIG 300
    
    * Updates
    
    * Update index.mdx
    
    * Add banner
    
    * Update index.mdx
    
    ---------
    
    Co-authored-by: Mikhail Volkov <mikhail@volkovlabs.io>

--- config/theme.config.ts
+++ config/theme.config.ts
@@ -56,4 +56,11 @@ export const THEME_CONFIG: ThemeConfig = {
       "markup-templating", // Markup templating (e.g., HTML)
     ],
   },
+
+  // Announcement banner
+  announcementBar: {
+    id: "business_intelligence_3_0",
+    content:
+      '<a href="/blog/business-intelligence-3.0.0">Business Intelligence 3.0.0</a> is out 🚀',
+  },
 };
