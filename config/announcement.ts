4311d014dcb19e9d4e6fbc7b460e4c0fcfbf0a78
Daria Volkova <94922548+daria-vl@users.noreply.github.com>
2025-05-01 16:25:02 -0400

    Add Business Links animation (#963)
    * links-120-newgif
    
    * Update announcement.ts
    
    * Update index.mdx
    
    ---------
    
    Co-authored-by: Mikhail Volkov <mikhail@volkovlabs.io>

--- config/announcement.ts
+++ config/announcement.ts
@@ -5,4 +5,6 @@ export const ANNOUNCEMENT_BAR = {
   id: "business_intelligence_3_0",
   content:
     '<a href="/blog/business-intelligence-3.0.0">Business Intelligence 3.0.0</a> is out 🚀',
+  backgroundColor: "#6133bc",
+  textColor: "#FFFFFF",
 };
