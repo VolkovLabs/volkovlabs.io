c0ccd91ba0513be43353fed61e42a7a07e3de588
Mikhail Volkov <mikhail@volkovlabs.io>
2025-06-20 19:51:06 -0400

    Add BI 3.5.0 Blog

--- config/announcement.ts
+++ config/announcement.ts
@@ -4,7 +4,7 @@
 export const ANNOUNCEMENT_BAR = {
   id: "business_intelligence_3_4_0",
   content:
-    '<a href="/blog/business-intelligence-3.4.0/">Business Intelligence 3.4.0: Elevating Grafana Workflows with Visual Insights and Collaboration</a> is out 🚀',
+    '<a href="/blog/business-intelligence-3.5.0/">Business Intelligence 3.5.0: Elevating Grafana Workflows with Power and Precision</a> is out 🚀',
   backgroundColor: "#6133bc",
   textColor: "#FFFFFF",
 };
