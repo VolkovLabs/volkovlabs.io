51391094343c1b9223f70511e48bf105a4b1ad02
Mikhail Volkov <mikhail@volkovlabs.io>
2025-06-20 20:14:41 -0400

    Updates

--- config/announcement.ts
+++ config/announcement.ts
@@ -4,7 +4,7 @@
 export const ANNOUNCEMENT_BAR = {
   id: "business_intelligence_3_4_0",
   content:
-    '<a href="/blog/business-intelligence-3.5.0/">Business Intelligence 3.5.0: Elevating Grafana Workflows with Power and Precision</a> is out 🚀',
+    '<a href="/blog/business-intelligence-3.5.0/">Business Intelligence 3.5.0: 2x Faster Rendering, Enhanced Alerting, and UI Upgrades</a> is out 🚀',
   backgroundColor: "#6133bc",
   textColor: "#FFFFFF",
 };
