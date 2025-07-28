eb93eac4c164971f39a85361957f99435638b030
Mikhail Volkov <mikhail@volkovlabs.io>
2025-07-27 21:46:50 -0400

    Update thresholds

--- config/announcement.ts
+++ config/announcement.ts
@@ -2,9 +2,9 @@
  * Announcement configuration.
  */
 export const ANNOUNCEMENT_BAR = {
-  id: "business_intelligence_3_6_0",
+  id: "business_intelligence_3_7_0",
   content:
-    '<a href="/blog/business-intelligence-3.6.0/">Business Intelligence 3.6.0: Automated Reporting, Smarter Alerts, Faster Insights</a> is out 🚀',
+    '<a href="/blog/business-intelligence-3.7.0/">Business Intelligence 3.7.0: Elevating Insights with Innovation</a> is out 🚀',
   backgroundColor: "#6133bc",
   textColor: "#FFFFFF",
 };
